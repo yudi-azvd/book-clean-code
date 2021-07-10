14a8add4f5e622d8e57abb3a62bf273d84246b1f
Yudi Yamane <yudiyamane@gmail.com>
2021-07-10 20:44:06 -0300

    renamed package

--- src/chap14_SuccessiveRefinement/args_v1/ArgumentMarshaler.java
+++
@@ -1,7 +0,0 @@
-package chap14_SuccessiveRefinement.args_v1;
-
-import java.util.Iterator;
-
-public interface ArgumentMarshaler {
-  void set(Iterator<String> currentArgument) throws ArgsException;
-}
