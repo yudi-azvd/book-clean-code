9d1564d98117c08b58d62a2d04465ed1f4b7b21d
Yudi Yamane <yudiyamane@gmail.com>
2021-07-10 20:45:53 -0300

    added rough draft version

---
+++ src/chap14_SuccessiveRefinement/args_roughdraft/UseOfArgs.java
@@ -0,0 +1,25 @@
+package chap14_SuccessiveRefinement.args_roughdraft;
+
+
+public class UseOfArgs {
+  public static void main(String[] args) {
+    String[] realArgs = "-l -p 8080 -d /tmp/ ".split(" ");
+
+    try {
+      // deveria ser args, mas realArgs fica mais fácil de "testar"
+      Args arg = new Args("l,p#,d*", realArgs);
+      boolean logging = arg.getBoolean('l');
+      // int logging = arg.getInt('l');
+      int port = arg.getInt('p');
+      String directory = arg.getString('d');
+      // String[] words = arg.getStringArray('w');
+
+      System.out.printf(
+        "\n\nlogging %b | port %d | directory %s\n", 
+        logging, port, directory);
+    } 
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
